df6c921a51740375580f7afc62af8198a3cf462f
ysgmt <sugimoto@abelard.flet.keio.ac.jp>
2014-10-31 18:40:16 +0900

    Initial Commit

---
+++ ARExp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARExp
+//
+//  Created by Yutaro Sugimoto on 2014/10/31.
+//  Copyright (c) 2014年 y.sgmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARExp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARExp
+//
+//  Created by Yutaro Sugimoto on 2014/10/31.
+//  Copyright (c) 2014年 y.sgmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
